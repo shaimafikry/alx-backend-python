51d93e588939f35f4f1657bd0799caa8de4a9998
shaimaa <shaimafikry@yahoo.com>
2024-08-10 13:32:12 +0300

    add advanecd

--- 0x00-python_variable_annotations/100-safe_first_element.py
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -5,7 +5,9 @@ from typing import Any, List, Union, Sequence
 
 
 # The types of the elements of the input are not know
-def safe_first_element(lst: Sequence[Any])-> Union[Any, None]:
+def safe_first_element(lst: Sequence[Any]) -> Union[Any, None]:
+    """Augment the following code with the correct duck-typed annotations:
+"""
     if lst:
         return lst[0]
     else:
--- 0x00-python_variable_annotations/101-safely_get_value.py
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python3
-"""Given the parameters and the return values, add type annotations to the function
+"""Given the parameters and the return values,
+add type annotations to the function
 """
 from typing import Union, Any, Mapping, T
 
-def safely_get_value(dct: Mapping, key: Any, default: Union[T, None]) -> Union[Any, T]:
+
+def safely_get_value(dct: Mapping, key: Any,
+                     default: Union[T, None]) -> Union[Any, T]:
     """intdo to typing of maping and any
     """
     if key in dct:
