102914789ec98bf8145cc5d05167c321ce86f8d0
shaimaa <shaimafikry@yahoo.com>
2024-08-27 12:56:49 +0300

    add unittest

---
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+""" testing"""
+import unittest
+from parameterized import parameterized
+from utils import access_nested_map
+
+
+class TestAccessNestedMap(unittest.TestCase):
+    """ testing"""
+    @parameterized.expand([
+        ({"a": 1}, ("a",), 1),
+        ({"a": {"b": 2}}, ("a",), {"b": 2}),
+        ({"a": {"b": 2}}, ("a", "b"), 2)
+    ])
+    def test_access_nested_map(self,nested_map, path, output):
+        """ test the func"""
+        self.assertEqual(access_nested_map (nested_map, path), output)
+
+if __name__ == '__main__':
+    unittest.main()
