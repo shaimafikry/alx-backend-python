27a69d645150937534a781cf9906ad19a829bfdf
shaimaa <shaimafikry@yahoo.com>
2024-08-28 20:09:15 +0300

    add unittest

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -1,1 +1,28 @@
 #!/usr/bin/env python3
+""" testing"""
+import unittest
+from parameterized import parameterized
+from client import GithubOrgClient
+from unittest.mock import Mock, patch
+
+
+class TestGithubOrgClient(unittest.TestCase):
+    """ class test"""
+    # the func to be patched, the wanted return value
+    @parameterized.expand([
+        ("google", {"org": "res_org"}),
+        ("abc", {"org": "res_org"})
+    ])
+    @patch('client.get_json', return_value={"org": "res_org"})
+    def test_org(self, org_name, output, mock_obj):
+        """ test org"""
+        my_test = GithubOrgClient(org_name)
+        # make the call
+        self.assertEqual(my_test.org, output)
+        # check if it was called more than once
+        mock_obj.assert_called_once_with(
+          f"https://api.github.com/orgs/{org_name}")
+
+
+if __name__ == '__main__':
+    unittest.main()
